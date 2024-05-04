b602bfc5b44ff3f7f53f84d4b31bfaf015f85c0a
Tejdeepmaddileti <tejdeepmaddileti6281@gmail.com>
2024-05-04 17:15:06 +0530

    added project

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <footer>
+            <div
+              style={{
+                width:"100%",
+                padding:20,
+                minHeight:"20vh",
+                maxHeight:"30vh",
+                marginTop:60,
+              }}
+            >
+                <p style={{fontSize:"30px", textAlign:"center"}}>
+                    Built with passion by sathyabama student
+                </p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
